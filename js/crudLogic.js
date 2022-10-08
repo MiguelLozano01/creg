9d9bba1bdc8302ddad0996094f69e831c6c0ed9a
Miguel Lozano <miguelalozanod16@outlook.com>
2022-10-08 10:46:30 -0500

    final

---
+++ js/crudLogic.js
@@ -0,0 +1,23 @@
+document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
+
+
+function cargarJSON() {
+  fetch('datos.json')
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let html;
+      data.innerHTML = '';
+      data.forEach(function (precios){
+        html += `<tr>
+                 <th> ${precios.id}</th> 
+                 <th>${precios.ciudad}</th> 
+                 <th> ${precios.precio_gasolina}</th> 
+                 <th>${precios.precio_acpm}</th>
+                 </tr>`;
+      })
+      document.getElementById('table now').innerHTML = html;
+    })
+}
+
